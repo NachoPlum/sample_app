880f9a637eeb72e438453537e33c61a25e7e75e9
NachoPlum <ivallecillo@plum.com.ar>
2022-10-27 17:57:53 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@
 
   # Defines the root path route ("/")
   # root "articles#index"
-Rails.application.routes.draw do
-  root "static_pages#home"
+  Rails.application.routes.draw do
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+    root "static_pages#home"
 
-end
+    get "/help",      to: "static_pages#help"
+    get "/about",     to: "static_pages#about"
+    get "/contact",   to: "static_pages#contact"
+    get '/signup',    to: "users#new"
+  end
